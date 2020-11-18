4d5d41380d40b350a3f07e26857116d1d93b8cae
Ekaterina Koshevaya <uea06@mail.ru>
2020-11-19 00:16:18 +0400

    my project finnaly

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class App extends React.Component {
   constructor(){
     super();
      this.state={ mainHead:'Ты сегодня покормил кота?',
-                  cardsDatas : [{cardHead1Normal: 'Сказочное заморское яство', cardHead1Hover: 'Котэ не одобряет?', with_:"с фуагра",portion:"10 порций",  present:"мышь в подарок",   numb_kg:"0,5", bottom_head_normal:"Чего сидишь? Порадуй котэ,", bottom_head_disabled:" Печалька, с фуагра закончился", bottom_head_selected:"Печень утки разварная с артишоками", isSelected:false,  isDisabled:true,},
+                  cardsDatas : [{cardHead1Normal: 'Сказочное заморское яство', cardHead1Hover: 'Котэ не одобряет?', with_:"с фуагра",portion:"10 порций",  present:"мышь в подарок",   numb_kg:"0,5", bottom_head_normal:"Чего сидишь? Порадуй котэ,", bottom_head_disabled:" Печалька, с фуагра закончился", bottom_head_selected:"Печень утки разварная с артишоками", isSelected:false,  isDisabled:false,},
                                 {cardHead1Normal: 'Сказочное заморское яство', cardHead1Hover: 'Котэ не одобряет?',with_:"с рыбой", portion:"40 порций",  present:"2 мыши в подарок", numb_kg:"2", bottom_head_normal:"Чего сидишь? Порадуй котэ,", bottom_head_disabled:" Печалька, с рыбой закончился",   bottom_head_selected:"Головы щучьи с чесноком да свежайшая семушка", isSelected:false,  isDisabled:false},
-                                {cardHead1Normal: 'Сказочное заморское яство', cardHead1Hover: 'Котэ не одобряет?',with_:"с курой", portion:"100 порций", present:"5 мышей в подарок",numb_kg:"5", bottom_head_normal:"Чего сидишь? Порадуй котэ," , bottom_head_disabled:" Печалька, с курой закончился",   bottom_head_selected:"Филе из цыплят с трюфелями в бульоне", isSelected:false,  isDisabled:false,}],
+                                {cardHead1Normal: 'Сказочное заморское яство', cardHead1Hover: 'Котэ не одобряет?',with_:"с курой", portion:"100 порций", present:"5 мышей в подарок",numb_kg:"5", bottom_head_normal:"Чего сидишь? Порадуй котэ," , bottom_head_disabled:" Печалька, с курой закончился",   bottom_head_selected:"Филе из цыплят с трюфелями в бульоне", isSelected:false,  isDisabled:true}],
                 }
   }//constructor
   
